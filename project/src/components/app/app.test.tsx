tyort/guess-melody-8

9.13. Добавит тест для проверки маршрутизации.
Попробуем написать ещё более сложный тест и проверить в нём маршрутизацию.
При смене маршрута должна отрисовываться соответствующая страница
приложения. Некоторые компоненты подключены к хранилищу, поэтому
придётся подготовить моки. Для решения этой задачи воспользуемся
`configureStore` из пакета `redux-mock-store`.

Фактически мы будем тестировать компонент `App`. Его придётся немного
отрефакторить. Откажемся от применения в нём компонента `Router`.
Перенесём его на уровень выше (в `index.js`). Это необходимо, чтобы
мы могли замокать `history` и использовать Memory History.

project/src/components/app/app.test.tsx
@@ -1,0 +1,70 @@
+import {render, screen} from '@testing-library/react';
+import {Router} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {MAX_MISTAKE_COUNT, AuthorizationStatus, AppRoute} from '../../const';
+import App from './app';
+
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  USER: {authorizationStatus: AuthorizationStatus.Auth},
+  DATA: {isDataLoaded: true},
+  GAME: {step: 10, mistakes: 2},
+});
+
+const history = createMemoryHistory();
+const fakeApp = (
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>
+);
+
+describe('Application Routing', () => {
+  it('should render "WelcomeScreen" when user navigate to "/"', () => {
+    history.push(AppRoute.Root);
+    render(fakeApp);
+
+    expect(screen.getByText(/Начать игру/i)).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(`Можно допустить ${MAX_MISTAKE_COUNT}`, 'i'))).toBeInTheDocument();
+  });
+
+  it('should render "AuthScreen" when user navigate to "/login"', () => {
+    history.push(AppRoute.Login);
+    render(fakeApp);
+
+    expect(screen.getByText(/Сыграть ещё раз/i)).toBeInTheDocument();
+    expect(screen.getByText(/Хотите узнать свой результат\? Представьтесь!/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Логин/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Пароль/i)).toBeInTheDocument();
+  });
+
+  it('should render "WinScreen" when user navigate to "/result"', () => {
+    history.push(AppRoute.Result);
+    render(fakeApp);
+
+    expect(screen.getByText(/Вы настоящий меломан!/i)).toBeInTheDocument();
+    expect(screen.getByText(/Вы ответили правильно на 8 вопросов/i)).toBeInTheDocument();
+    expect(screen.getByText(/Сыграть ещё раз/i)).toBeInTheDocument();
+  });
+
+  it('should render "GameOverScreen" when user navigate to "/lose"', () => {
+    history.push(AppRoute.Lose);
+    render(fakeApp);
+
+    expect(screen.getByText(/Какая жалость!/i)).toBeInTheDocument();
+    expect(screen.getByText(/Попробовать ещё раз/i)).toBeInTheDocument();
+    expect(screen.getByText(/У вас закончились все попытки. Ничего, повезёт в следующий раз!/i)).toBeInTheDocument();
+  });
+
+  it('should render "NotFoundScreen" when user navigate to non-existent route', () => {
+    history.push('/non-existent-route');
+    render(fakeApp);
+
+    expect(screen.getByText('404. Page not found')).toBeInTheDocument();
+    expect(screen.getByText('Вернуться на главную')).toBeInTheDocument();
+  });
+});

ShkurinskayaIrina/guess-melody-demo

project/src/components/app/app.test.tsx
@@ -1,0 +1,76 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import HistoryRouter from '../history-route/history-route';
+import {MAX_MISTAKE_COUNT, AuthorizationStatus, AppRoute} from '../../const';
+import App from './app';
+
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  USER: {authorizationStatus: AuthorizationStatus.Auth},
+  DATA: {isDataLoaded: true},
+  GAME: {step: 10, mistakes: 2},
+});
+
+const history = createMemoryHistory();
+
+const fakeApp = (
+  <Provider store={store}>
+    <HistoryRouter history={history}>
+      <App />
+    </HistoryRouter>
+  </Provider>
+);
+
+describe('Application Routing', () => {
+  it('should render "WelcomeScreen" when user navigate to "/"', () => {
+    history.push(AppRoute.Root);
+
+    render(fakeApp);
+
+    expect(screen.getByText(/Начать игру/i)).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(`Можно допустить ${MAX_MISTAKE_COUNT}`, 'i'))).toBeInTheDocument();
+  });
+
+  it('should render "AuthScreen" when user navigate to "/login"', () => {
+    history.push(AppRoute.Login);
+
+    render(fakeApp);
+
+    expect(screen.getByText(/Сыграть ещё раз/i)).toBeInTheDocument();
+    expect(screen.getByText(/Хотите узнать свой результат\? Представьтесь!/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Логин/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Пароль/i)).toBeInTheDocument();
+  });
+
+  it('should render "WinScreen" when user navigate to "/result"', () => {
+    history.push(AppRoute.Result);
+
+    render(fakeApp);
+
+    expect(screen.getByText(/Вы настоящий меломан!/i)).toBeInTheDocument();
+    expect(screen.getByText(/Вы ответили правильно на 8 вопросов/i)).toBeInTheDocument();
+    expect(screen.getByText(/Сыграть ещё раз/i)).toBeInTheDocument();
+  });
+
+  it('should render "GameOverScreen" when user navigate to "/lose"', () => {
+    history.push(AppRoute.Lose);
+
+    render(fakeApp);
+
+    expect(screen.getByText(/Какая жалость!/i)).toBeInTheDocument();
+    expect(screen.getByText(/Попробовать ещё раз/i)).toBeInTheDocument();
+    expect(screen.getByText(/У вас закончились все попытки. Ничего, повезёт в следующий раз!/i)).toBeInTheDocument();
+  });
+
+  it('should render "NotFoundScreen" when user navigate to non-existent route', () => {
+    history.push('/non-existent-route');
+
+    render(fakeApp);
+
+    expect(screen.getByText('404. Page not found')).toBeInTheDocument();
+    expect(screen.getByText('Вернуться на главную')).toBeInTheDocument();
+  });
+});